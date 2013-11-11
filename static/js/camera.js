34be2d55a4035fa2678a8b88cc94a49260a44621
Kara <klouie06@gmail.com>
2013-11-10 21:49:43 -0800

    made it work

--- static/js/camera.js
+++ static/js/camera.js
@@ -1,6 +1,6 @@
 var camera = (function() {
 	var options;
-	var video, canvas, context;
+	var video, canvas, canvasOverlay, context;
 	var renderTimer
 
 	function initVideoStream() {
@@ -32,23 +32,23 @@ var camera = (function() {
 
 	function initCanvas() {
 		canvas = options.targetCanvas || document.createElement("canvas");
+		canvas.id = "livevideo";
 		vid.appendChild(canvas)
 		canvas.setAttribute('width', options.width);
 		canvas.setAttribute('height', options.height);
 		context = canvas.getContext('2d');
 
 	    canvasOverlay = document.createElement("canvas");
-	    canvasOverlay.setAttribute('width', 320);
-	    canvasOverlay.setAttribute('height', 240);
+	    canvasOverlay.setAttribute('width', options.width);
+	    canvasOverlay.setAttribute('height', options.height);
 	    canvasOverlay.style.position = "absolute";
-	    canvasOverlay.style.top = '0px';
+	    canvasOverlay.style.left = '10px';
 	    canvasOverlay.style.zIndex = '100001';
 	    // canvasOverlay.style.display = 'block';
 	    overlayContext = canvasOverlay.getContext('2d');
-	    overlayContext.clearRect(0,0,320,240);
+	    overlayContext.clearRect(0,0,900,600);
+	    overlayContext.id = "overlay";
 	    vid.appendChild(canvasOverlay);
-
-
 
 		if (options.mirror) {
 			context.translate(canvas.width, 0);
@@ -63,7 +63,34 @@ var camera = (function() {
 
 		renderTimer = setInterval(function() {
 			try {
+				// videoOnCanvas.fromCanvas(canvas, options, canvasOverlay);
 				context.drawImage(video, 0, 0, video.width, video.height);
+				context.beginPath();
+		        context.arc(150,150,50,0,Math.PI*2,true); // Outer circle
+		        context.moveTo(185,150);
+		        context.fillStyle = "rgba(255,255,0,1)";
+		        context.fill();
+		        context.arc(150,150,35,0,Math.PI,false);   // Mouth (clockwise)
+		        context.moveTo(140,140);
+		        context.arc(135,140,5,0,Math.PI*2,true);  // Left eye
+		        // context.fillStyle = "black";
+		        // context.fillArc();
+		        context.moveTo(170,140);
+		        context.arc(165,140,5,0,Math.PI*2,true);  // Right eye
+		        context.stroke();
+
+
+		        // speech bubble
+		        context.beginPath();
+		        context.moveTo(75,25);
+		        context.quadraticCurveTo(25,25,25,62.5);
+		        context.quadraticCurveTo(25,100,50,100);
+		        context.quadraticCurveTo(50,120,80,125);
+		        context.quadraticCurveTo(60,120,65,100);
+		        context.quadraticCurveTo(125,100,125,62.5);
+		        context.quadraticCurveTo(125,25,75,25);
+		        context.stroke();
+				// overlayContext.drawImage,
 				options.onFrame(canvas);
 			} catch (e) {
 				//TODO
--- static/js/find_color_on_canvas.js
+++ static/js/find_color_on_canvas.js
@@ -1,84 +1,81 @@
 var videoOnCanvas = (function() {
-	function drawOnCanvas (canvas, options) {
+	
+  function drawOnCanvas (canvas, options) {
 		var canvasWidth = canvas.width;
 		var canvasHeight = canvas.height;
-        var context = canvas.context("2d");
+    var context = canvas.context("2d");
 
 		var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
 		for (var y = 0; y < canvasHeight; y++) {
 			for (var x = 0; x < canvasWidth; x++) {
 				var offset = (y * canvasWidth + x) * 4;
 
-				getColorAtOffset(imageData, offset);
+				ctx.fillStyle = getColorAtOffset(imageData, offset);
 				ctx.fillRect(x, y, 1, 1);
 			}
 		}
-    }
+  }
 
-    function drawOnOverlay (canvas, options) {
-        var canvasWidth = canvas.width;
-        var canvasHeight = canvas.height;
-        var context = canvas.context("2d");
+  function drawOnOverlay (canvas, options) {
+      var canvasWidth = canvas.width;
+      var canvasHeight = canvas.height;
+      var context = canvas.context("2d");
 
-        var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-          ctx.beginPath();
-          ctx.arc(150,150,50,0,Math.PI*2,true); // Outer circle
-          ctx.moveTo(185,150);
-          ctx.fillStyle = "rgba(255,255,0,0.5)";
-          ctx.fill();
-          ctx.arc(150,150,35,0,Math.PI,false);   // Mouth (clockwise)
-          ctx.moveTo(140,140);
-          ctx.arc(135,140,5,0,Math.PI*2,true);  // Left eye
-          // ctx.fillStyle = "black";
-          // ctx.fillArc();
-          ctx.moveTo(170,140);
-          ctx.arc(165,140,5,0,Math.PI*2,true);  // Right eye
-          ctx.stroke();
-
-
-          // speech bubble
-          ctx.beginPath();
-          ctx.moveTo(75,25);
-          ctx.quadraticCurveTo(25,25,25,62.5);
-          ctx.quadraticCurveTo(25,100,50,100);
-          ctx.quadraticCurveTo(50,120,80,125);
-          ctx.quadraticCurveTo(60,120,65,100);
-          ctx.quadraticCurveTo(125,100,125,62.5);
-          ctx.quadraticCurveTo(125,25,75,25);
-          ctx.stroke();
+      var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+        ctx.beginPath();
+        ctx.arc(150,150,50,0,Math.PI*2,true); // Outer circle
+        ctx.moveTo(185,150);
+        ctx.fillStyle = "rgba(255,255,0,1)";
+        ctx.fill();
+        ctx.arc(150,150,35,0,Math.PI,false);   // Mouth (clockwise)
+        ctx.moveTo(140,140);
+        ctx.arc(135,140,5,0,Math.PI*2,true);  // Left eye
+        // ctx.fillStyle = "black";
+        // ctx.fillArc();
+        ctx.moveTo(170,140);
+        ctx.arc(165,140,5,0,Math.PI*2,true);  // Right eye
+        ctx.stroke();
 
 
-            }
-        }
-    }
+        // speech bubble
+        ctx.beginPath();
+        ctx.moveTo(75,25);
+        ctx.quadraticCurveTo(25,25,25,62.5);
+        ctx.quadraticCurveTo(25,100,50,100);
+        ctx.quadraticCurveTo(50,120,80,125);
+        ctx.quadraticCurveTo(60,120,65,100);
+        ctx.quadraticCurveTo(125,100,125,62.5);
+        ctx.quadraticCurveTo(125,25,75,25);
+        ctx.stroke();
+  }
 
-    function draw() {
-        drawOnCanvas(canvas, options);
-        drawOnOverlay(canvasOverlay, options);
-        return
-    }
-	
-	function getColorAtOffset(data, offset) {
-                return {
-                        red: data[offset],
-                        green: data[offset + 1],
-                        blue: data[offset + 2],
-                        alpha: data[offset + 3]
-        		};
-    }
+  function draw(canvas, options, canvasOverlay) {
+      drawOnCanvas(canvas, options);
+      drawOnOverlay(canvasOverlay, options);
+      return;
+  }
 
-    function bound(value, interval) {
-    	return Math.max(interval[0], Math.min(interval[1], value));
-    }
+  function getColorAtOffset(data, offset) {
+              return {
+                      red: data[offset],
+                      green: data[offset + 1],
+                      blue: data[offset + 2],
+                      alpha: data[offset + 3]
+      		};
+  }
 
-    return {
-    	fromCanvas: function(canvas, options) {
-    		options = options || {};
-    		options.contrast = (typeof options.contrast === "undefined" ? 128 : options.contrast);
-    		options.callback = options.callback || doNothing;
+  function bound(value, interval) {
+  	return Math.max(interval[0], Math.min(interval[1], value));
+  }
+  return {
+  	fromCanvas: function(canvas, options, canvasOverlay) {
+  		options = options || {};
+  		options.contrast = (typeof options.contrast === "undefined" ? 128 : options.contrast);
+  		// options.callback = options.callback || doNothing;
+      draw(canvas, options, canvasOverlay);
 
-    		return draw();
-    	}
-    };
+      return;
+  	}
+  };
 
 })();
