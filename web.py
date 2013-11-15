b69f550b5beb8c64625587df728ce16964e4e36f
Kara <klouie06@gmail.com>
2013-11-14 16:54:06 -0800

    played with threejs

--- web.py
+++ web.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template("/index.html")
 
+@app.route("/threejs")
+def threejs():
+	return render_template("/threejssample.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
